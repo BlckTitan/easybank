256ce121627e2ff85068fafb63abc0bf6abab951
BlckTitan <dorshman406@gmail.com>
2023-05-09 17:16:35 +0100

    Project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
