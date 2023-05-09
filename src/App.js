a94620ff0ca1a19388691dd612fa4ac07ddda4dc
BlckTitan <dorshman406@gmail.com>
2023-05-09 17:39:37 +0100

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
-
 function App() {
   return (
     <div className="App">
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-5xl font-bold underline">
+        Hello world!!!
+      </h1>
     </div>
   );
 }
