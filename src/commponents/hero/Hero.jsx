32cd49fef2d0226eedb987510d140f95f51c2b91
BlckTitan <dorshman406@gmail.com>
2023-05-11 17:37:59 +0100

    Adjusted th button alignment of the hero section and the height of the valueService_text

--- src/commponents/hero/Hero.jsx
+++ src/commponents/hero/Hero.jsx
@@ -7,13 +7,13 @@ export default function Hero() {
   return (
     <div className='hero_container w-full flex justify-center'>
         <header className='wrapper flex flex-col-reverse xl:flex-row xl:justify-between'>
-           <section className='heroText w-full xl:w-2/5  h-4/6 xl:h-full pt-8 xl:pt-0 flex flex-col justify-center items-start'>
-            <h1 className='text-3xl xl:text-5xl mb-4 text-center md:text-left'>Next generation digital banking</h1>
-            <p className='text-lg mb-4 text-center md:text-left'>
-                Take your financial life online.
-                Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
-            </p>
-            <button className='button_gradient w-40 h-14 xl:h-12 rounded-full flex justify-center items-center'>Request invite</button>
+           <section className='heroText w-full xl:w-2/5  h-4/6 xl:h-full pt-20 xl:pt-0 flex flex-col items-center md:items-start'>
+              <h1 className='text-3xl xl:text-5xl mb-4 text-center md:text-left'>Next generation digital banking</h1>
+              <p className='text-lg mb-4 text-center md:text-left'>
+                  Take your financial life online.
+                  Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
+              </p>
+              <button className='button_gradient w-40 h-14 xl:h-12 rounded-full flex justify-center items-center'>Request invite</button>
            </section>
            <section className='heroImage w-full xl:w-3/5 h-2/6 mb-4 xl:mt-0 xl:mb-0 xl:h-full flex justify-end items-center relative'>
             <img src={imgHero} alt="hero img; format: png;"/>
