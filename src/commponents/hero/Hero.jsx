3c690bca95a0305a55928ce129658673372e4170
BlckTitan <dorshman406@gmail.com>
2023-05-11 15:13:19 +0100

    Made the hero content responsive for small devices

--- src/commponents/hero/Hero.jsx
+++ src/commponents/hero/Hero.jsx
@@ -5,16 +5,16 @@ import imgHero from '../../images/image-mockups.png';
 export default function Hero() {
   return (
     <div className='hero_container w-full flex justify-center'>
-        <header className='wrapper flex flex-col xl:flex-row xl:justify-between'>
-           <section className='heroText w-full xl:w-2/5  h-full flex flex-col justify-center items-start'>
-            <h1 className='text-5xl mb-4'>Next generation digital banking</h1>
+        <header className='wrapper flex flex-col-reverse xl:flex-row xl:justify-between'>
+           <section className='heroText w-full xl:w-2/5  h-4/6 xl:h-full pt-8 xl:pt-0 flex flex-col justify-center items-start'>
+            <h1 className='text-3xl xl:text-5xl mb-4'>Next generation digital banking</h1>
             <p className='text-lg mb-4'>
                 Take your financial life online.
                 Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
             </p>
             <button className='button_gradient w-40 h-14 xl:h-12 rounded-full flex justify-center items-center'>Request invite</button>
            </section>
-           <section className='heroImage w-full xl:w-3/5 h-full flex justify-end items-center relative'>
+           <section className='heroImage w-full xl:w-3/5 h-2/6 mb-4 xl:mt-0 xl:mb-0 xl:h-full flex justify-end items-center relative'>
             <img src={imgHero} alt="hero img; format: png;"/>
            </section>
         </header>
